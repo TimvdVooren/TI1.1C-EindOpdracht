8862e330a0782f575fab9411747c00e8a1860132
Unknown <tvooren1@avans.nl>
2018-10-01 12:35:59 +0200

    [Started] server/client application

---
+++ EindopdrachtRickEnTim/PongClient/PongClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PongClient
+{
+    class PongClient
+    {
+        public PongClient()
+        {
+            TcpClient client = new TcpClient();
+            client.Connect("145.49.50.116", 1506);
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
---
+++ EindopdrachtRickEnTim/PongServer/PongServer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PongServer
+{
+    class PongServer
+    {
+        private int PlayerCount = 0;
+
+        public PongServer()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 1506);
+            listener.Start();
+
+            while (PlayerCount < 2)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                PlayerCount++;
+
+                Thread thread = new Thread(HandleClient(client));
+                thread.Start();
+            }
+        }
+
+        private ThreadStart HandleClient(TcpClient client)
+        {
+            Console.WriteLine("Client connected");
+            while (true)
+            {
+
+            }
+        }
+    }
+}
