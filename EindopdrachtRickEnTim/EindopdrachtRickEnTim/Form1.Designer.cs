a83499c89c82f0e7b13b694f0aa167dc7ef89c0d
Unknown <tvooren1@avans.nl>
2018-10-08 12:42:45 +0200

    [Added] gui

--- EindopdrachtRickEnTim/EindopdrachtRickEnTim/Form1.Designer.cs
+++ EindopdrachtRickEnTim/EindopdrachtRickEnTim/Form1.Designer.cs
@@ -28,13 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.friendList = new System.Windows.Forms.ListBox();
+            this.sendTextBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.receiveTextBox = new System.Windows.Forms.RichTextBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // friendList
+            // 
+            this.friendList.Dock = System.Windows.Forms.DockStyle.Left;
+            this.friendList.FormattingEnabled = true;
+            this.friendList.ItemHeight = 16;
+            this.friendList.Location = new System.Drawing.Point(0, 0);
+            this.friendList.Name = "friendList";
+            this.friendList.Size = new System.Drawing.Size(181, 528);
+            this.friendList.TabIndex = 0;
+            // 
+            // sendTextBox
+            // 
+            this.sendTextBox.Location = new System.Drawing.Point(3, 3);
+            this.sendTextBox.Name = "sendTextBox";
+            this.sendTextBox.Size = new System.Drawing.Size(778, 75);
+            this.sendTextBox.TabIndex = 1;
+            this.sendTextBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(787, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 75);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.sendTextBox);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(181, 450);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(861, 78);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // receiveTextBox
+            // 
+            this.receiveTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.receiveTextBox.Location = new System.Drawing.Point(181, 0);
+            this.receiveTextBox.Name = "receiveTextBox";
+            this.receiveTextBox.ReadOnly = true;
+            this.receiveTextBox.Size = new System.Drawing.Size(861, 450);
+            this.receiveTextBox.TabIndex = 4;
+            this.receiveTextBox.Text = "";
+            this.receiveTextBox.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1042, 528);
+            this.Controls.Add(this.receiveTextBox);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.friendList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AppChat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox friendList;
+        private System.Windows.Forms.RichTextBox sendTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.RichTextBox receiveTextBox;
     }
 }
 
